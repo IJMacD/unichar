1f85a63705642c9f13a97ae2f6ea0a7691bc7e98
Iain MacDonald <IJMacD@gmail.com>
2019-04-05 14:40:55 +0800

    Updates

---
+++ precache-manifest.e07f3b2d88a9bb651481649c648ced4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd98d8eab30a925cdd2a",
+    "url": "/unichar/static/css/main.261eb620.chunk.css"
+  },
+  {
+    "revision": "fd98d8eab30a925cdd2a",
+    "url": "/unichar/static/js/main.9f6c78c4.chunk.js"
+  },
+  {
+    "revision": "e40bb5b6a36be3580538",
+    "url": "/unichar/static/js/runtime~main.10472662.js"
+  },
+  {
+    "revision": "ffeda4aed0053eec05f6",
+    "url": "/unichar/static/js/2.cef8d966.chunk.js"
+  },
+  {
+    "revision": "349b7ccee31c0a8089dbbb9c86762b4a",
+    "url": "/unichar/index.html"
+  }
+];
