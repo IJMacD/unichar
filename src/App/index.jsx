e1421f855ff90e1a12266f6c66fe5af4ba0eecfa
Iain MacDonald <iain.ilearner@gmail.com>
2019-04-23 18:16:53 +0800

    Add bit styling

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -297,7 +297,7 @@ function Bytes (props) {
     const bytes = [...utf8.encode(String.fromCodePoint(props.value))].map(c => c.charCodeAt(0));
 
     return <div className={classes.byte} style={{ marginRight: 4 }}>
-      <div>{bytes.map((b, i) => <span key={i}>{b.toString(16).padStart(2,'0')}</span>)}</div>
+      {bytes.map((b, i) => <span key={i}>{b.toString(16).padStart(2,'0')}</span>)}
     </div>;
   } catch (e) {
     return;
@@ -311,7 +311,26 @@ function BinaryBytes (props) {
     const bytes = [...utf8.encode(String.fromCodePoint(props.value))].map(c => c.charCodeAt(0));
 
     return <div className={classes.byte + " " + classes.binaryByte} style={{ marginRight: 4 }}>
-      {bytes.map((b, i) => <span key={i}>{b.toString(2).padStart(8,'0')}</span>)}
+      {bytes.map((b, i, a) => {
+        const bString = b.toString(2).padStart(8, '0');
+        let bytePrefix, byteData;
+        if (a.length === 1) {
+          bytePrefix = bString.substr(0, 1);
+          byteData = bString.substr(1);
+        } else {
+          if (i === 0) {
+            bytePrefix = bString.substr(0, a.length + 1);
+            byteData = bString.substr(a.length + 1);
+          } else  {
+            bytePrefix = bString.substr(0,2);
+            byteData = bString.substr(2);
+          }
+        }
+        return <span key={i}>
+          <span className={classes.bytePrefix}>{bytePrefix}</span>
+          <span className={classes.byteData}>{byteData}</span>
+        </span>;
+      })}
     </div>
   } catch (e) {
     return;
