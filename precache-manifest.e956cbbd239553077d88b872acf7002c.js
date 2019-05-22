e4ec0f33cc7f45f8da333274b4c3a2a63e47c30d
Iain MacDonald <IJMacD@gmail.com>
2019-05-22 09:36:32 +0800

    Updates

--- precache-manifest.e956cbbd239553077d88b872acf7002c.js
+++ precache-manifest.e956cbbd239553077d88b872acf7002c.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/unichar/static/js/runtime~main.b12249da.js"
   },
   {
-    "revision": "b073fdec1fe89a6e63ae",
-    "url": "/unichar/static/js/main.884d79b9.chunk.js"
+    "revision": "4f0604f18c80468a161a",
+    "url": "/unichar/static/js/main.4c1ee359.chunk.js"
   },
   {
     "revision": "864d29ba039738c1bf28",
     "url": "/unichar/static/js/3.acc0c9a9.chunk.js"
   },
   {
-    "revision": "9300d4f1d3e14ab8b947",
-    "url": "/unichar/static/js/2.94f54277.chunk.js"
+    "revision": "51ec3c870cb1d6a0852e",
+    "url": "/unichar/static/js/2.ca075531.chunk.js"
   },
   {
-    "revision": "b073fdec1fe89a6e63ae",
+    "revision": "4f0604f18c80468a161a",
     "url": "/unichar/static/css/main.943f5d93.chunk.css"
   },
   {
-    "revision": "30db6fea436cb5fb30cf1c1e3ee0220d",
+    "revision": "d40229eed93e96d68359191ac9b8c8d4",
     "url": "/unichar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/unichar/precache-manifest.259c33d4f4064d32dbd0ae58d7bbe96e.js"
+  "/unichar/precache-manifest.e956cbbd239553077d88b872acf7002c.js"
 );
 
 workbox.clientsClaim();
