fbc41f6857a903b4cd9b018f37b848f774c9830e
Iain MacDonald <iain.ilearner@gmail.com>
2019-09-19 11:57:15 +0800

    Updates

--- precache-manifest.18c6ed0e33b0b68ef31db432ab4b0d8a.js
+++ precache-manifest.18c6ed0e33b0b68ef31db432ab4b0d8a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ce854100f80a173769d3",
-    "url": "/unichar/static/css/main.8f80b18c.chunk.css"
+    "revision": "1b690f22992f060bf5a7",
+    "url": "/unichar/static/css/main.ccd0cd84.chunk.css"
   },
   {
-    "revision": "ce854100f80a173769d3",
-    "url": "/unichar/static/js/main.47d23ea2.chunk.js"
+    "revision": "1b690f22992f060bf5a7",
+    "url": "/unichar/static/js/main.16a019eb.chunk.js"
   },
   {
     "revision": "799e16537a4e744c6d33",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/unichar/static/js/3.e9c9598c.chunk.js"
   },
   {
-    "revision": "319e308dd097b000b3e883a1bff53e68",
+    "revision": "9539ab3796d6a4f8d34a3a6a79ac70ea",
     "url": "/unichar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/unichar/precache-manifest.913849b43f3e90499404ad2cb5312aef.js"
+  "/unichar/precache-manifest.18c6ed0e33b0b68ef31db432ab4b0d8a.js"
 );
 
 workbox.clientsClaim();
